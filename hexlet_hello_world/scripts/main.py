24ec86e8e8e052cfbdb41003bf6c73b1b07134b8
Anton Korkischenko <antonjesus1337@gmail.com>
2025-12-29 18:59:47 +0300

    test

---
+++ hexlet_hello_world/scripts/main.py
@@ -0,0 +1,8 @@
+from hexlet_hello_world.module import greet
+
+def main():
+    greet()
+
+
+if __name__ == "__main__":
+    main()
---
+++ hexlet_hello_world/scripts/second_script.py
@@ -0,0 +1,8 @@
+from hexlet_hello_world.scripts.main import main
+
+
+def second_main():
+    main()
+
+
+second_main()
